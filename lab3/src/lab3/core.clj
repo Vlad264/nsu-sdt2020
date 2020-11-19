aef271f38b02a950a1f3a716252eb330adb17946
Vlad264 <vlad-498@yandex.ru>
2020-11-19 19:37:24 +0700

    Create lab3: task2 with tests

--- lab3/src/lab3/core.clj
+++ lab3/src/lab3/core.clj
@@ -1,6 +1,5 @@
 (ns lab3.core
-  (:use lab3.task1))
+  (:use lab3.task1)
+  (:use lab3.task2))
 
-(defn -main [& args]
-  ;(println (my-partition 3 (range 10))))
-  (println (task1 even? 1 (range 100))))
+(defn -main [& args])
--- lab3/test/lab3/core_test.clj
+++ lab3/test/lab3/core_test.clj
@@ -47,4 +47,49 @@
     (time (doall (lab3.task1/task1 even? 9 (range 10000))))
     (time (doall (lab3.task1/task1 even? 10 (range 10000))))
     (println)))
+    
+(deftest task2-test
+  (testing "task2"
+    (is (lab3.task2/task2 even? 1 3 (range 100)) (filter even? (range 100)))
+    (is (lab3.task2/task2 even? 2 3 (range 100)) (filter even? (range 100)))
+    (is (lab3.task2/task2 even? 3 3 (range 100)) (filter even? (range 100)))
+    (is (lab3.task2/task2 even? 4 3 (range 100)) (filter even? (range 100)))
+    (is (lab3.task2/task2 even? 5 3 (range 100)) (filter even? (range 100)))
+    (is (lab3.task2/task2 even? 6 3 (range 100)) (filter even? (range 100)))
+    (is (lab3.task2/task2 even? 7 3 (range 100)) (filter even? (range 100)))
+    (is (lab3.task2/task2 even? 8 3 (range 100)) (filter even? (range 100)))
+    (is (lab3.task2/task2 even? 9 3 (range 100)) (filter even? (range 100)))
+    (is (lab3.task2/task2 even? 10 3 (range 100)) (filter even? (range 100)))))
+
+(deftest task2-infinite-test
+  (testing "task2-infinite"
+    (is (take 1000 (lab3.task2/task2 even? 1 3 (iterate inc 0))) (take 1000 (filter even? (iterate inc 0))))
+    (is (take 1000 (lab3.task2/task2 even? 2 3 (iterate inc 0))) (take 1000 (filter even? (iterate inc 0))))
+    (is (take 1000 (lab3.task2/task2 even? 3 3 (iterate inc 0))) (take 1000 (filter even? (iterate inc 0))))
+    (is (take 1000 (lab3.task2/task2 even? 4 3 (iterate inc 0))) (take 1000 (filter even? (iterate inc 0))))
+    (is (take 1000 (lab3.task2/task2 even? 5 3 (iterate inc 0))) (take 1000 (filter even? (iterate inc 0))))
+    (is (take 1000 (lab3.task2/task2 even? 6 3 (iterate inc 0))) (take 1000 (filter even? (iterate inc 0))))
+    (is (take 1000 (lab3.task2/task2 even? 7 3 (iterate inc 0))) (take 1000 (filter even? (iterate inc 0))))
+    (is (take 1000 (lab3.task2/task2 even? 8 3 (iterate inc 0))) (take 1000 (filter even? (iterate inc 0))))
+    (is (take 1000 (lab3.task2/task2 even? 9 3 (iterate inc 0))) (take 1000 (filter even? (iterate inc 0))))
+    (is (take 1000 (lab3.task2/task2 even? 10 3 (iterate inc 0))) (take 1000 (filter even? (iterate inc 0))))))
+
+(deftest task2-time-test
+  (testing "task2-time"
+    (println)
+    (println "Test filter time wint (take 1000) (iterate inc 0)")
+    (time (doall (take 1000 (filter even? (iterate inc 0)))))
+    (println)
+    (println "Test task2 time wint (take 1000) (iterate inc 0) from 1 to 10 threads")
+    (time (doall (take 1000 (lab3.task2/task2 even? 1 3 (iterate inc 0)))))
+    (time (doall (take 1000 (lab3.task2/task2 even? 2 3 (iterate inc 0)))))
+    (time (doall (take 1000 (lab3.task2/task2 even? 3 3 (iterate inc 0)))))
+    (time (doall (take 1000 (lab3.task2/task2 even? 4 3 (iterate inc 0)))))
+    (time (doall (take 1000 (lab3.task2/task2 even? 5 3 (iterate inc 0)))))
+    (time (doall (take 1000 (lab3.task2/task2 even? 6 3 (iterate inc 0)))))
+    (time (doall (take 1000 (lab3.task2/task2 even? 7 3 (iterate inc 0)))))
+    (time (doall (take 1000 (lab3.task2/task2 even? 8 3 (iterate inc 0)))))
+    (time (doall (take 1000 (lab3.task2/task2 even? 9 3 (iterate inc 0)))))
+    (time (doall (take 1000 (lab3.task2/task2 even? 10 3 (iterate inc 0)))))
+    (println)))
 
